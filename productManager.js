a26b40ec77ffd6e08c76e8227075b7f462013261
Paolo Ferro <pgff_9@hotmail.com>
2023-11-12 12:44:11 -0300

    Segundo Desafio Entregable

--- productManager.js
+++ productManager.js
@@ -1,90 +1,124 @@
-class ProductManager {
-    constructor() {
-      this.products = [];
-      this.productIdCounter = 1;
-    }
+const fs = require('fs');
 
-    //Agrega producto si todos los campos existen y si el codigo no se repite
-    addProduct(product) {
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+    this.nextId = 1; // Variable autoincrementable (ID)
+    this.products = [];
+    this.loadProducts();
+  }
+
+  //Carga los productos que haya en el archivo.json
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf8');
+      this.products = JSON.parse(data);
+      this.updateNextId();
+    } catch (error) {
+      console.error('El archivo de productos no existe');
+    }
+  }
+
+  //Actualiza el ID para que el siguiente producto agregado tenga un ID unico y sea mayor a los ID existentes
+  updateNextId() {
+    if (this.products.length > 0) {
+      const maxId = Math.max(...this.products.map(product => product.id));
+      this.nextId = maxId + 1;
+    }
+  }
+
+  //Guarda los productos en el archivo.json
+  saveProducts() {
+    fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2), 'utf8');
+  }
+
+  //Agrega producto si todos los campos existen y si el codigo no se repite
+  addProduct(product) {
         //Si un campo no se agrega muestra el error
-      if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
+    if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
         console.error('Todos los campos son obligatorios.');
         return;
       }
         //Si el campo "code" esta repetido muestra el error
-      if (this.products.some((existingProduct) => existingProduct.code === product.code)) {
+    if (this.products.some((existingProduct) => existingProduct.code === product.code)) {
         console.error('El campo "code" ya está en uso.');
         return;
       }
-        //Si todo esta correcto se agrega el producto
-      product.id = this.productIdCounter++;
-      this.products.push(product);
+    const newProduct = {
+      ...product,
+      id: this.nextId++
+    };
+    this.products.push(newProduct);
+    this.saveProducts();
+    return newProduct;
+  }
+
+  //Retorna todos los productos
+  getProducts() {
+    return this.products;
+  }
+
+  //Retorna un producto por su ID y muestra el error si el producto no se encuentra o no existe
+  getProductById(id) {
+    const product = this.products.find(product => product.id === id);
+    if (!product) {
+      throw new Error('El producto no fue encontrado.');
     }
-    
-    //Retorna todos los productos
-    getProducts() {
-      return this.products;
-    }
-  
-    //Retorna un producto por si ID
-    getProductById(id) {
-      const product = this.products.find((p) => p.id === id);
-      if (!product) {
-        console.error('Producto no encontrado.');
-      }
-      return product;
+    return product;
+  }
+
+  //Modifica un producto segun el ID
+  updateProduct(id, updatedFields) {
+    const index = this.products.findIndex(product => product.id === id);
+    if (index !== -1) {
+      this.products[index] = { ...this.products[index], ...updatedFields, id };
+      this.saveProducts();
+      return this.products[index];
     }
   }
-  
-  // Ejemplos:
-  const productManager = new ProductManager();
 
-  //Producto uno completo
-  productManager.addProduct({
-    title: 'Silla',
-    description: 'Sirve para sentarte',
-    price: 19.99,
-    thumbnail: 'silla.jpg',
-    code: 'PROD1',
-    stock: 100,
-  });
+  //Elimina un producto segun el ID, si el producto no existe muestra el error
+  deleteProduct(id) {
+    const index = this.products.findIndex(product => product.id === id);
+    if (index !== -1) {
+      const deletedProduct = this.products.splice(index, 1)[0];
+      this.saveProducts();
+      return deletedProduct;
+    } else {
+      throw new Error('El producto no fue encontrado.');
+    }
+  }
+}
 
-  //Producto dos completo
-  productManager.addProduct({
-    title: 'Peine',
-    description: 'Sirve para peinarte',
-    price: 4.99,
-    thumbnail: 'peine.jpg',
-    code: 'PROD2',
-    stock: 50,
-  });
+// Ejemplo de uso (COMENTAR LOS FRAGMENTOS DE CODIGO QUE HAGAN FALTA PARA PODER TESTEAR BIEN CADA PARTE):
+const productManager = new ProductManager('productos.json');
 
-  //Producto tres incompleto
-  productManager.addProduct({
+// Agrega un producto, se puede poner incompleto para testear el error o con "code" repetido para testear el error al intentar agregar un producto
+const newProduct = {
     title: 'Cuchillo',
     description: 'Sirve para cortar',
-    price: 9.99,
-    code: 'PROD3',
-  });
+    price: 4.99,
+    thumbnail: 'cuchillo.jpg',
+    code: 'PROD4',
+    stock: 60,
+};
+productManager.addProduct(newProduct);
 
-  //Producto cuatro con mismo codigo que producto uno
-  productManager.addProduct({
-    title: 'Telefono',
-    description: 'Sirve para llamar gente',
-    price: 49.99,
-    thumbnail: 'telefono.jpg',
-    code: 'PROD1',
-    stock: 150,
-  });
+//Muestra todos los productos
+const allProducts = productManager.getProducts();
+console.log('Todos los productos:', allProducts);
 
-  //Testeo para obtener todos los productos
-  const allProducts = productManager.getProducts();
-  console.log(allProducts);
+const productId = 4;//VARIABLE ID PARA PODER TESTEAR DISTINTAS FUNCIONES
 
-  //Testeo para obtener un producto por el ID
-  const productById = productManager.getProductById(2);
-  console.log(productById);
+//Muestra un producto por ID
+const productById = productManager.getProductById(productId);
+console.log(`Producto con ID ${productId}:`, productById);
 
-  //Testeo para obtener un producto que no existe
-  const nonExistentProduct = productManager.getProductById(10);
-  
+//Actualiza un producto por ID
+const updatedFields = { price: 39.99, stock: 32 };
+const updatedProduct = productManager.updateProduct(productId, updatedFields);
+console.log('Producto actualizado:', updatedProduct);
+
+//Elimina un producto por ID
+const deletedProduct = productManager.deleteProduct(productId);
+console.log('Producto eliminado:', deletedProduct);
