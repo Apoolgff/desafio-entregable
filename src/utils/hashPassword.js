85835f90378ce56f2744d7dfd995bc6b8dad2e8f
Paolo Ferro <pgff_9@hotmail.com>
2024-01-15 12:20:12 -0300

    bcrypt password

--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -2,4 +2,4 @@ const bcrypt = require('bcrypt')
 
 exports.createHash = async password => await bcrypt.hash(password, bcrypt.genSalt(10))
 
-exports.isValidPassword = () => {}
+exports.isValidPassword = async (user, password) => await bcrypt.compare(password, user.password)
